78b7eb00f69f40986638fdcadce9a720677f9aae
Paarth S Rathore <47487547+RathoreSPaarth@users.noreply.github.com>
2020-08-16 00:49:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ let txt = document.querySelector('#txt')
 let btn = document.querySelector('#btn')
 
 btn.addEventListener('click',()=>{
-    axios.get('https://xlit.quillpad.in/quillpad_backend2/processWordJSON?lang=hindi&inString='+txt.value, {
-        mode: 'no-cors',
+    axios.get('://xlit.quillpad.in/quillpad_backend2/processWordJSON?lang=hindi&inString='+txt.value, {
+//         mode: 'no-cors',
         headers:{
             'Access-Control-Allow-Origin' : '*'
         } 
