971c00b198a52c7271295b31829236126a63a5c0
Paarth S Rathore <47487547+RathoreSPaarth@users.noreply.github.com>
2020-08-15 23:33:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+let txt = document.querySelector('#txt')
+let btn = document.querySelector('#btn')
+
+btn.addEventListener('click',()=>{
+    fetch('xlit.quillpad.in/quillpad_backend2/processWordJSON?lang=hindi&inString='+txt.value, {
+        headers:{
+            'Access-Control-Allow-Origin' : '*'
+        } 
+    })
+    .then((result)=>{
+        console.log(result)
+    })
+    
+})
+
+// async function language(){
+//     let result = await fetch('xlit.quillpad.in/quillpad_backend2/processWordJSON?lang=hindi&inString='+txt.value)
+//     .then((result)=>{
+//         console.log(result)
+//     })
+// }
+// headers:{
+//     "Access-Control-Request-Headers" : "*",
+// "Access-Control-Request-Method" : "*"
+// }
